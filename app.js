5cfa36e28348cb919c78b18fb68831225c578bf7
Addison Welp <welpalw@gmail.com>
2024-08-07 15:19:00 -0400

    Modified JS and HTML

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ function startCountdown() {
   var vacationDateStart= new Date(document.getElementById("dateInput").value);
   var offsetTime = vacationDateStart.getTimezoneOffset();
   console.log(vacationDateStart, offsetTime);
+  
+//trying to figure out how to store this data so a client doesn't have to re-enter it
 
   var vacationDateStart = vacationDateStart.getTime() + (offsetTime * 60000);
   var currentDate = new Date().getTime(); 
+  //adding line 11 to try and resolve line 6 note
+  localStorage.setItem('vacationDateStart', vacationDateStart);
+var timeRemaining = vacationDateStart - currentDate;
 
-  var timeRemaining = vacationDateStart - currentDate;
 
   const seconds = 1000
   const minutes = seconds * 60;
@@ -23,6 +27,13 @@ function startCountdown() {
   + textMinutes + " Minutes " + textSeconds + " Seconds ";
 
 }
+//adding lines 31-38 to see if this will call the stored date
+document.addEventListener('DOMContentLoaded', function() {
+  const storedDate = localStorage.getItem('VacationDateStart');
+  if (storedDate) {
+     document.getElementById('dateInput').value = storedDate;
+  }
+});
 
 setInterval(startCountdown, 1000);
 
